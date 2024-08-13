c8ac924dfde2959877fbafbb728be25689484e5a
Viola_Pioggia <114391708+ViolaPioggia@users.noreply.github.com>
2024-08-13 23:52:52 +0800

    Create hello.thrift

---
+++ hello.thrift
@@ -0,0 +1,14 @@
+namespace go Hello
+
+struct HelloRequest {
+    1: string message
+}
+
+struct HelloResponse {
+    1: string message
+}
+
+
+service HelloService {
+    HelloResponse hello(1: HelloRequest req)
+}
